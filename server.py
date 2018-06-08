3b72dc4ece1502ec72e8e36dfc6a72a916cb4e36
Michael <vmichaelkirkaldy@live.com>
2018-06-08 10:49:00 -0500

    final updates

--- server.py
+++ server.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')  
 
+#sets method to POST
 @app.route('/survey', methods=['POST']) 
 def survey():
+	#sets variable equal to form request in html attribute, i.e <div name="name"></div>
 	name = request.form['name']
 	locations = request.form['locations']
 	languages = request.form['languages']
 	comments = request.form['comments']
+	#returns .html and the variables that were assigned to values to make page, dynamic
 	return render_template('survey.html', name=name, locations=locations, languages=languages, comments=comments) 
 
 
