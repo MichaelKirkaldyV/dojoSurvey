62558741c8b584626f08002e7f0ab3bc9c0673a0
Michael <vmichaelkirkaldy@live.com>
2018-06-08 09:36:27 -0500

    Updates to all files

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template  # Import Flask to allow us to create our app, and import
-                                          # render_template to allow us to render index.html.
-app = Flask(__name__)                     # Global variable __name__ tells Flask whether or not we
-                                          # are running the file directly or importing it as a module.
-@app.route('/')                           # The "@" symbol designates a "decorator" which attaches the
-                                          # following function to the '/' route. This means that
-                                          # whenever we send a request to localhost:5000/ we will run
-                                          # the following "hello_world" function.
+from flask import Flask, render_template, request, redirect
+                                          
+app = Flask(__name__)                     
+                                          
+@app.route('/')                                                             
 def index():
-  return render_template('index.html')    # Render the template and return it!
+  return render_template('index.html')  
+
+@app.route('/survey', methods=['POST']) 
+def survey():
+	print "Summary:"
+	name = request.form['name']
+	locations = request.form['locations']
+	languages = request.form['languages']
+	comments = request.form['comments']
+	return redirect('/') 
+
+
 app.run(debug=True) 
